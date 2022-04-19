a18c489c0da2b36919aaebf5929a6036478b4435
s.pavlinina <sonya.pav25@gmail.com>
2022-04-19 23:23:32 +0300

    Add: header component

---
+++ src/components/Header/UserInfo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const UserInfo = () => (
+    <div className="user-info">
+        <div className="name">Dev Dude</div>
+        <div className="status">
+            <span className="circle" />Online
+        </div>
+    </div>
+);
+
+export default UserInfo;
