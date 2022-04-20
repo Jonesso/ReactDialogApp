56205b43f9de675aeebc6b147216c33958309b1c
s.pavlinina <sonya.pav25@gmail.com>
2022-04-20 22:11:29 +0300

    Add: dialog components

---
+++ src/data.js
@@ -0,0 +1,63 @@
+const dialog = [
+    {
+        avatar:
+            "https://sun9-2.userapi.com/c638729/v638729951/1d0ea/f9V7aJyh6tw.jpg",
+        message: "Hi",
+        id: 0,
+        date: "2020-10-11T10:09:04.712Z",
+        is: "her",
+    },
+    {
+        avatar:
+            "https://sun9-2.userapi.com/c638729/v638729951/1d0ea/f9V7aJyh6tw.jpg",
+        message: "What's up?",
+        id: 1,
+        date: "2020-10-11T10:19:04.712Z",
+        is: "her",
+    },
+    {
+        avatar:
+            "https://sun9-2.userapi.com/c638729/v638729951/1d0ea/f9V7aJyh6tw.jpg",
+        message: "Just came back from work",
+        id: 2,
+        date: "2020-10-11T10:24:04.712Z",
+        is: "her",
+    },
+    {
+        avatar:
+            "https://sun9-58.userapi.com/c836638/v836638514/867c/SPMigNB8gw0.jpg",
+        message: "Hi! All's good, you?",
+        id: 3,
+        date: "2020-10-11T11:09:04.712Z",
+        status: "sended",
+        is: "my",
+    },
+    {
+        avatar:
+            "https://sun9-58.userapi.com/c836638/v836638514/867c/SPMigNB8gw0.jpg",
+        message: "Cool",
+        id: 4,
+        date: "2020-10-11T13:01:04.712Z",
+        status: "readed",
+        is: "my",
+    },
+    {
+        avatar:
+            "https://sun9-58.userapi.com/c836638/v836638514/867c/SPMigNB8gw0.jpg",
+        message: "Is everything OK?",
+        id: 5,
+        date: "2020-10-11T14:09:07.712Z",
+        status: "readed",
+        is: "my",
+    },
+    {
+        avatar:
+            "https://sun9-2.userapi.com/c638729/v638729951/1d0ea/f9V7aJyh6tw.jpg",
+        message: "Yup, it is. C U soon",
+        id: 6,
+        date: "2020-10-11T15:09:04.712Z",
+        is: "her",
+    },
+];
+
+export default dialog;
