a1ce12d4a7f8b4fcd7643f1660004fa05f03912b
s.pavlinina <sonya.pav25@gmail.com>
2022-04-21 11:44:49 +0300

    Add: message sending & deleting

--- src/components/Sender/index.js
+++ src/components/Sender/index.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import "./styles.css";
+import PropTypes from "prop-types";
 
-const Sender = () => {
+const Sender = ({ onAddMessage }) => {
     const [value, setValue] = useState("");
 
     const onChange = (event) => setValue(event.target.value);
     const onSubmit = (event) => {
         event.preventDefault();
+
+        onAddMessage({
+            id: Date.now(),
+            avatar:
+                "https://sun9-58.userapi.com/c836638/v836638514/867c/SPMigNB8gw0.jpg",
+            message: value,
+            date: new Date().toISOString(),
+            is: "my",
+            status: "sent",
+        });
+        setValue("");
     };
 
     return (
@@ -17,9 +29,13 @@ const Sender = () => {
                 onChange={onChange}
                 required
             />
-            <button>Отправить</button>
+            <button>Send</button>
         </form>
     );
+};
+
+Sender.propTypes = {
+    onAddMessage: PropTypes.func.isRequired,
 };
 
 export default Sender;
